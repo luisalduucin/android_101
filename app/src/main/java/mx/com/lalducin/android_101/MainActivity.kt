9c102f547e5ec78272d11414281f6a416a45ca12
luisalduucin <lalducin@nearsoft.com>
2017-07-13 13:20:10 -0500

    Added first codelab for activity lifecycle

--- app/src/main/java/mx/com/lalducin/android_101/MainActivity.kt
+++ app/src/main/java/mx/com/lalducin/android_101/MainActivity.kt
@@ -2,11 +2,17 @@ package mx.com.lalducin.android_101
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("LIFE_CYClE", "OnCreate")
     }
+
+    //TODO Override the rest of the lifecycle states and print them
+
 }
