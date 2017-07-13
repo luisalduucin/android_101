ed3a48324ae08faa6379c625834aab87dc489870
luisalduucin <lalducin@nearsoft.com>
2017-07-13 14:25:26 -0500

    Overrided activity lifecycle methods

--- app/src/main/java/mx/com/lalducin/android_101/MainActivity.kt
+++ app/src/main/java/mx/com/lalducin/android_101/MainActivity.kt
@@ -1,7 +1,7 @@
 package mx.com.lalducin.android_101
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +10,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("LIFE_CYClE", "OnCreate")
+        Log.d("LIFE_CYCLE", "OnCreate")
     }
 
-    //TODO Override the rest of the lifecycle states and print them
+    // TODO
+    // 1.- Override the rest of the lifecycle states and log them
+    // 2.- Implement common actions on some lifecycle states
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d("LIFE_CYCLE", "OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("LIFE_CYCLE", "OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d("LIFE_CYCLE", "OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d("LIFE_CYCLE", "OnStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("LIFE_CYCLE", "OnDestroy")
+    }
 
 }
