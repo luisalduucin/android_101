a9bc00e5ec7d3381254128acc82bbf35105bd383
luisalduucin <lalducin@nearsoft.com>
2017-07-19 03:04:04 -0500

    Removed unused code

--- app/src/main/java/mx/com/lalducin/android_101/MainActivity.kt
+++ app/src/main/java/mx/com/lalducin/android_101/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         addButton.setOnClickListener { _ -> startActivity(Intent(this, TripDetail::class.java)) }
 
         Log.d("MAIN_ACTIVITY", "OnCreate")
