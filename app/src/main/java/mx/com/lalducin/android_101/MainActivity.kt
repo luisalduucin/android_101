5f72fbdd8b09cdb05c68da0d2bc2e990012a8d64
luisalduucin <lalducin@nearsoft.com>
2017-07-19 02:56:12 -0500

    Added TODOS for Lifecycle Codelab

--- app/src/main/java/mx/com/lalducin/android_101/MainActivity.kt
+++ app/src/main/java/mx/com/lalducin/android_101/MainActivity.kt
@@ -1,8 +1,10 @@
 package mx.com.lalducin.android_101
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +12,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("LIFE_CYClE", "OnCreate")
+        addButton.setOnClickListener { _ -> startActivity(Intent(this, TripDetail::class.java)) }
+
+        Log.d("MAIN_ACTIVITY", "OnCreate")
     }
 
     //TODO Override the rest of the lifecycle states and print them
--- app/src/main/java/mx/com/lalducin/android_101/TripDetail.kt
+++ app/src/main/java/mx/com/lalducin/android_101/TripDetail.kt
@@ -2,6 +2,7 @@ package mx.com.lalducin.android_101
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_trip_detail.*
 
 class TripDetail : AppCompatActivity() {
@@ -10,6 +11,13 @@ class TripDetail : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trip_detail)
         setSupportActionBar(toolbar)
+
+        Log.d("TRIP_DETAIL", "OnCreate")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("TRIP_DETAIL", "OnDestroy")
     }
 
 }
